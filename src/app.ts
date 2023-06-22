fed60c650606c1161641dade820bd2cefe226b41
Isaiah Clifford Opoku <opokuisaiahclifford123@gmail.com>
2023-06-22 20:16:12 +0000

    Adding of first function about the forech mehtod

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,10 @@
-console.log('Hello World!');
+const numbers : number[] = [1, 2, 3, 4, 5];
+
+numbers.forEach((number:number, index :number, array)   => {
+  console.log(number, index, array);
+});
+// out put : 1 0 [ 1, 2, 3, 4, 5 ]
+// out put : 2 1 [ 1, 2, 3, 4, 5 ]
+// out put : 3 2 [ 1, 2, 3, 4, 5 ]
+// out put : 4 3 [ 1, 2, 3, 4, 5 ]
+// out put : 5 4 [ 1, 2, 3, 4, 5 ]
