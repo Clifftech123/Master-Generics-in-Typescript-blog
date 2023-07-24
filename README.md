db84c77fd7aa80ca28fbd6ddfb02f490a8f84b7b
Isaiah Clifford Opoku <opokuisaiahclifford123@gmail.com>
2023-07-24 01:39:18 +0000

    Adding of content

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Blog  Post to master  Typescript Generics: How to write a function that accepts any array of objects and returns the first item in the array
+
+-This repo contain all the source code be using in the blog post
--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,67 @@
-const func = <T>  (  First: T ) : T =>  {
-    return First  ;
+// syntax
+// IN typescript, we use <> to define the generic type
+function identity<T>(arg: T): T {
+  return arg;
+}
+// call the generic function with number type
+
+let output = identity<number>(123);
+console.log(output); // 5
+
+// call the generic function with string type
+
+let output2 = identity<string>("myString");
+console.log(output2); // myString
+
+// infer the type from the value
+
+let output3 = identity(123234);
+console.log(output3); // 123
+
+
+
+// ======
+// Generic Functions 
+// =================
+
+
+function genericFunctions<T>(arg: T[]): T[] {
+  console.log(arg.length);
+  return arg;
 }
 
+//Call the generic function with number type
 
-const func2 = <T>   (Love: T): T => {
-  return Love;
+let outputFuncNumbers = genericFunctions<number>([1, 2, 3, 4, 5]);
+console.log(outputFuncNumbers);
+
+//Call the generic function with string type
+
+let output5 = genericFunctions<string>(["Apple", "Orange", "Banana"]);
+console.log(output5);
+
+
+// Generic classes
+
+class GenericClass<T> {
+  private genericProperty: T;
+
+  constructor(value: T) {
+    this.genericProperty = value;
+  }
+
+  public getGenericProperty(): T {
+    return this.genericProperty;
+  }
 }
 
+// create instance of generic class with number type argument
+
+let instance1 = new GenericClass<number>(123);
+console.log(instance1.getGenericProperty());
+
+
+// create instance of generic class with string type argument
+
+let instance2 = new GenericClass<string>("Hello, world");
+console.log(instance2.getGenericProperty());
